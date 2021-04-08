63424141737dd92324d52c74338de5b01c8efb44
Fabian Margreiter <fabian.margreiter@gmail.com>
2021-04-08 18:01:00 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there, you dick! And fuck you!
+          Hi there, you dick! And fuck you! <br>
+			 P.S.: You really suck.
         </p>
         <a
           className="App-link"
